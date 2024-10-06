5451e052ab2eda7993673c2aa9beb89b28cd20f4
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2024-10-06 19:17:27 +0530

    Event list generated through mapping function and addition of dropdown to filter events based on events

--- amand_pune/src/pages/EventPage.jsx
+++ amand_pune/src/pages/EventPage.jsx
@@ -2,19 +2,14 @@ import { useLocation } from "react-router-dom";
 
 const EventPage = () => {
   const location = useLocation();
-  const {
-    eventHeader,
-    eventDesc,
-    eventImage,
-    eventLongDesc,
-    eventGalleryLink,
-  } = location.state || {};
+  const { eventName, eventDesc, eventImage, eventLongDesc, eventGalleryLink } =
+    location.state || {};
 
   return (
     <div className="bg-bgColor flex flex-col gap-[3rem] items-center justify-center py-2 px-4 md:px[11.5rem] min-h-screen">
       <img src={eventImage} className="w-[50rem]" />
       <div className="text-2xl font-semibold text-center font-mont">
-        {eventHeader}
+        {eventName}
       </div>
       <div className="w-full md:w-[60rem] text-[14px] font-charter">
         {eventDesc}
